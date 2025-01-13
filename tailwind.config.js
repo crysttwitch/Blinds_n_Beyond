2d9e97f614c0c41d398b76701d346ccd1182b547
crysttwitch <147266137+crysttwitch@users.noreply.github.com>
2025-01-14 00:02:35 +0530

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html",
+  content: ["./index.html","./assets/pictures/blinds.jpeg",
+                           "./assets/pictures/blindsnbeyondlogo.png",
+                           "./assets/pictures/curtains.jpeg",
+                           "./assets/pictures/product1.png",
+                           "./assets/pictures/product2.png",
     "./**/*.{html,js}" ],
   theme: {
     extend: {},
